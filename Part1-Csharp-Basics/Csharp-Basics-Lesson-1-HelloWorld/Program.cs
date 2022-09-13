16189892839fa220318c694b06d230a8c579d320
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-13 22:09:55 +0300

    Add files via upload
    Added Lesson 1

---
+++ Part1-Csharp-Basics/Csharp-Basics-Lesson-1-HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Basics_Lesson_1_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Acoptex!");
+            Console.Read();
+        }
+    }
+}
