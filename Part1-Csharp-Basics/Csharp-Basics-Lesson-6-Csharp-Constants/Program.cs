e33282c62416e541dd9c9cf9eb76ef288608b667
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-15 22:22:00 +0300

    Add files via upload
    Added Lesson 6

---
+++ Part1-Csharp-Basics/Csharp-Basics-Lesson-6-Csharp-Constants/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Csharp_Basics_Lesson_6_Csharp_Constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // C# constants
+
+            //const int num;
+
+            const int num = 1977;
+
+            // num = 2;
+
+            Console.WriteLine(num);
+
+            Console.Read();
+        }
+    }
+}
