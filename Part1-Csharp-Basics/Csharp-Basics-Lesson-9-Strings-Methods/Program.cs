ce82dc04d2b181ed32561d25fa109e7f744bda80
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-17 14:58:52 +0300

    Update Program.cs

--- Part1-Csharp-Basics/Csharp-Basics-Lesson-9-Strings-Methods/Program.cs
+++ Part1-Csharp-Basics/Csharp-Basics-Lesson-9-Strings-Methods/Program.cs
@@ -9,12 +9,15 @@ namespace Csharp_Basics_Lesson_9_Strings_Methods
             // String methods
 
             string greeting = "Hello, World!";
-            Console.WriteLine();
+
             Console.WriteLine($"The length of the {greeting} is: {greeting.Length} symbols");
 
             Console.WriteLine(greeting.ToUpper());   // Outputs "HELLO, WORLD!"
+            
             Console.WriteLine(greeting.ToLower());   // Outputs "hello, world!"
-
+            
+            Console.WriteLine(greeting[0]);  // Outputs "H"
+            Console.WriteLine(myString[7]);  // Outputs "W"
             Console.Read();
         }
     }
