f6510bdb49c7efdf922bba2a484f17d78f1bfbd9
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-17 14:55:53 +0300

    Add files via upload
    Added Lesson 9

---
+++ Part1-Csharp-Basics/Csharp-Basics-Lesson-9-Strings-Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Csharp_Basics_Lesson_9_Strings_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // String methods
+
+            string greeting = "Hello, World!";
+            Console.WriteLine();
+            Console.WriteLine($"The length of the {greeting} is: {greeting.Length} symbols");
+
+            Console.WriteLine(greeting.ToUpper());   // Outputs "HELLO, WORLD!"
+            Console.WriteLine(greeting.ToLower());   // Outputs "hello, world!"
+
+            Console.Read();
+        }
+    }
+}
