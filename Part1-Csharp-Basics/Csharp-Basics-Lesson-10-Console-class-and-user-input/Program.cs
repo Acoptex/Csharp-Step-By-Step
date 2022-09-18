834b70eb0d99b1c89a0e2cbf7c933ede4c73ec87
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-18 14:29:24 +0300

    Add files via upload
    Added Lesson 10

---
+++ Part1-Csharp-Basics/Csharp-Basics-Lesson-10-Console-class-and-user-input/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Csharp_Basics_Lesson_10_Console_class_and_user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console class and user input
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.Write("Type any text and press Enter: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine($"You have typed -> {userInput}");
+
+            Console.WriteLine("****************************************");
+
+            Console.WriteLine("Type the string and hit Enter key: ");
+            int anciValue = Console.Read();
+            Console.WriteLine($"ANCII value is {anciValue}");
+
+            Console.WriteLine("****************************************");
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
