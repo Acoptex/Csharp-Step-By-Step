181d2368d4b1e7e6a04232cc1b6c90cbd45e33dc
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-18 14:38:46 +0300

    Update Program.cs

--- Part1-Csharp-Basics/Csharp-Basics-Lesson-4-Naming-Conventions-And-Coding-Standards/Program.cs
+++ Part1-Csharp-Basics/Csharp-Basics-Lesson-4-Naming-Conventions-And-Coding-Standards/Program.cs
@@ -18,22 +18,34 @@ namespace Csharp_Basics_Lesson_4_Naming_Conventions_And_Coding_Standards
                Names are case sensitive("myNum" and "mynum" are different variables)
                Reserved words (like C# keywords, for example int, double, float, for and so on) cannot be used as names
             */
-
-            int a = 1;
-
-            int b2 = 2;
+            // Correct
+            int x = 1;
 
             int num_one = 3;
 
             bool _isRegister = true;
 
+            string firstName;
+            
+            int firstNumber;
+            
+            bool isFunny;
+            
+            // Avoid
+            // String firstName;
+            // Int32 firstNumber;
+            // Boolean isFunny;
+
             // Coding standards
 
             /*
-             Reasonable variable name
-             Proper method name
+             Reasonable variable name, for example, firstValue
+             Proper method name, for example, CalculationResults
+             Proper class name, for example, MyActivity
+             Proper method arguments, for example, firstName
              */
-           
+                     
+            //int 1num =12; // Don't use numbers at the start of variable names
             int revolutionPerMinute = 60; // Good                       
             int rpm = 60; // It's OK, but not so easy to understand what rpm actually is
 
