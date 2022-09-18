4881dae589eb039452355b662f0967a3d981186b
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-18 14:55:57 +0300

    Add files via upload
    Added Lesson 11

---
+++ Part1-Csharp-Basics/Csharp-Basics-Lesson-11-Parsing-string-to-integer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Csharp_Basics_Lesson_11_Parsing_string_to_integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parsing strings to integers
+
+            // 1. First method
+
+            Console.Write("Enter your age: ");
+            // int userAge1 = Console.ReadLine(); -> error message: Cannot implicitly convert type 'string' to 'int'
+            int userAge1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Your age is " + userAge1);
+            Console.WriteLine("***********************************************************************************************************");
+            string stringResult = "25" + "20";
+            int integerResult = Int32.Parse("25") + Int32.Parse("20");
+            Console.WriteLine("Result from strings concatination - {0} and result from sum of numbers - {1}", stringResult, integerResult);
+            Console.WriteLine("***********************************************************************************************************");
+
+            // 2. Second method
+
+            Console.Write("Enter your age: ");
+            int userAge2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is " + userAge2);
+
+            Console.Read();
+        }
+    }
+}
