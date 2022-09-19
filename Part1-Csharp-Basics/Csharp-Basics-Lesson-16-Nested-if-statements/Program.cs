25785a570564adc66a5477b0b39782dab05db346
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-19 17:58:10 +0100

    Add files via upload
    Added Lesson 16

---
+++ Part1-Csharp-Basics/Csharp-Basics-Lesson-16-Nested-if-statements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Csharp_Basics_Lesson_16_Nested_if_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nested if statements
+
+            bool isRegistered = true;
+
+            Console.Write("Username: ");
+            string userName = Console.ReadLine();
+           
+            if (isRegistered)
+            {
+                Console.WriteLine("You are registered.");
+
+                if (userName != "")
+                {
+                    Console.WriteLine($"Welcome, {userName}");
+
+                    // This code will be executed, if userName is "Alex"
+                    if (userName.Equals("Admin"))
+                    {
+                        Console.WriteLine("Hi Admin.");
+                    }
+                }
+            }
+
+            // You will get the same result by using logical AND
+            if (isRegistered && userName != "" && userName.Equals("Admin"))
+            {
+                Console.WriteLine("You are registered.");
+                Console.WriteLine($"Hello {userName}");
+                Console.WriteLine("Hi Admin.");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
