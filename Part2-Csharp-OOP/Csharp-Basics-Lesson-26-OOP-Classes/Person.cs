e330d78c7307eacbd22fac0a68882a7eabcef91d
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-21 17:22:11 +0100

    Add files via upload
    Added Lesson 26

---
+++ Part2-Csharp-OOP/Csharp-Basics-Lesson-26-OOP-Classes/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Basics_Lesson_26_OOP_Classes
+{
+    // This class is a blueprint for a datatype
+
+    internal class Person
+    {
+        // Class Members
+        // fields (or attributes)
+        //string firstName = "Alex";    // private variable, you can not access it
+        public string firstName = "Sam";// public variable, default value
+        public string lastName;         // public variable
+        
+        // member method
+        public void IntroduceMyself()
+        {
+            Console.WriteLine($"Hi, my name is {firstName}");
+        }
+    }   
+           
+     
+    
+
+}
---
+++ Part2-Csharp-OOP/Csharp-Basics-Lesson-26-OOP-Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Csharp_Basics_Lesson_26_OOP_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // A class is a template for objects, and an object is an
+            // instance of a class.
+            // When the individual objects are created, they inherit
+            // all the variables and methods from the class.
+
+            Person alex = new Person();
+            alex.firstName = "Alex";
+            alex.lastName = "Best";
+            alex.IntroduceMyself();
+
+            Person sam = new Person();
+            sam.firstName = "Sam";
+            sam.lastName = "Best";
+            sam.IntroduceMyself();
+
+            Console.Read();
+
+        }
+    }
+}
