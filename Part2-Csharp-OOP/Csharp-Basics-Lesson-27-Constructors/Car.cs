cfaffcb8cd986552a1491d3fa6318e9440c1fdc1
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-21 17:23:25 +0100

    Add files via upload
    Added Lesson 27

---
+++ Part2-Csharp-OOP/Csharp-Basics-Lesson-27-Constructors/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Basics_Lesson_27_Constructors
+{
+    internal class Car
+    {
+        // Create the fields
+        string carName; // private variables
+        string carModel;
+        int carYear;
+        string carColor;
+
+        // Contructor's name should be the same as class name
+        // Create a constructor without parameters for the Car class
+        // Default constructor
+        public Car()
+        {
+            Console.WriteLine("I am called from constructor without parameters");
+        }
+        // Create a class constructor with multiple parameters for the Car class
+        public Car(string carName, string carModel)
+        {
+            this.carName = carName;
+            this.carModel = carModel;
+            Console.WriteLine("I am called from constructor with parameters");
+            Console.WriteLine($"The car is {carName} {carModel}");
+        }
+        //public Car(string carName1, string carModel1)
+        //{
+        //    carName = carName1;
+        //    carModel = carModel1;
+        //    Console.WriteLine("I am called from constructor with parameters");
+        //    Console.WriteLine($"The car is {carName} {carModel}");
+        //}
+        public Car(string carName, string carModel, int carYear)
+        {
+            this.carName = carName;
+            this.carModel = carModel;
+            this.carYear = carYear;
+            Console.WriteLine("I am called from constructor with parameters");
+            Console.WriteLine($"The car is {carYear} {carName} {carModel}");
+        }
+        public Car(string carName, string carModel, int carYear, string carColor)
+        {
+            this.carName = carName;
+            this.carModel = carModel;
+            this.carYear = carYear;
+            this.carColor=carColor;
+
+            Console.WriteLine("I am called from constructor with parameters");
+            Console.WriteLine($"The car is {carColor} {carYear} {carName} {carModel}");
+        }
+        // Member method
+        public void SimpleMethod()
+        {
+            Console.WriteLine("I am called from Simple method.");
+        }
+    }
+    
+   
+}
---
+++ Part2-Csharp-OOP/Csharp-Basics-Lesson-27-Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Csharp_Basics_Lesson_27_Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of the Car Class (this will call the constructor)
+            Car audi = new Car();
+            
+            Car ford = new Car("Ford", "X5");
+            // Console.WriteLine(ford.carName);// You can't access the value of carName as it is private
+
+            Car opel = new Car("Opel", "Astra", 2005);
+
+            Car nissan = new Car("Nissan", "Micra", 2015, "black");
+            
+            nissan.SimpleMethod();
+
+            Console.Read();
+
+        }
+    }
+}
