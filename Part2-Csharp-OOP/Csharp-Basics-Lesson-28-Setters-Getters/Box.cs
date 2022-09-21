37252d9fa25b4eb700e8373f667cd49db023a327
ALEKSANDR CUCHRIAJEV <113382837+Acoptex@users.noreply.github.com>
2022-09-21 19:21:28 +0100

    Add files via upload
    Added Lesson 28

---
+++ Part2-Csharp-OOP/Csharp-Basics-Lesson-28-Setters-Getters/Box.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Basics_Lesson_28_Setters_Getters
+{
+    internal class Box
+    {
+        // member variables
+        private int length = 8;
+        public int height;
+        public int width;
+        //public int volume; -> not required if you use the GetVolume()
+
+        // 1. Encapsulation. First way to access the private member variable
+        //public void SetLength(int length) 
+        //{
+        //    if (length < 0){
+        //        throw new Exception("Length can not be below 0");
+        //    }
+        //    this.length = length;
+        //}
+        private void SetLength(int length) // only access from Box class or derived classes
+        {
+            if (length < 0)
+            {
+                throw new Exception("Length can not be below 0");
+            }
+            this.length = length;
+        }
+        public int GetLength()
+        {
+            return this.length;
+        }
+        public int GetVolume()
+        {
+            return this.length * this.height * this.width;
+        }     
+
+
+        // Member method
+        public void DisplayInfo()
+        {
+            // Console.WriteLine($"The box volume: {length} x {width} x {height} = {volume = length * width * height}");
+            Console.WriteLine($"The box volume: {length} x {width} x {height} = {GetVolume()}");
+        }
+
+        
+
+    }
+}
---
+++ Part2-Csharp-OOP/Csharp-Basics-Lesson-28-Setters-Getters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Csharp_Basics_Lesson_28_Setters_Getters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box myBox = new Box();
+            // myBox.length = 5; // you can not access it if length member variable is set to private
+            // myBox.SetLength(-2); // you need a Set method to access it // if SetLength() is set to private - it's not possible to access
+
+            myBox.height = 4;
+            myBox.width = 5;
+
+            Console.WriteLine($"The length is {myBox.GetLength()}");
+            Console.WriteLine($"The volume of box is {myBox.GetVolume()}");
+
+            myBox.DisplayInfo();
+
+            Console.Read();
+
+        }
+    }
+}
